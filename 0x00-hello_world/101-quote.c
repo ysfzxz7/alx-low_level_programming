11420f116c9e31ad2902a7356190c19b2b98576b
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-13 06:22:38 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  *main- Entry point
  *
@@ -6,6 +7,13 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful\"- \
+Dora Korpar, 2015-10-19\n";
+	const int len = strlen(str);
+	int i =0;
+	do{
+		putchar((int) str[i]);
+		i++;
+	}while(i<len);
 return (1);
 }
