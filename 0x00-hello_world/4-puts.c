8b003b2941ac218479a7831be293c1c99926a34f
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-13 05:12:22 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *the main function is to print a phrase using puts() function
+ *main - Entry point
  *
- *
- *
+ *Return : Always 0 (Success)
  */
-	int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
