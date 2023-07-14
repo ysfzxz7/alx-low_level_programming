14911a545c0d799a7d68c45f12894f661fd0de12
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-14 07:43:46 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 57; digit++)
+	{
+	putchar(digit);
+	putchar(',');
+	putchar(' ');
+	}
+return (0);
+}
