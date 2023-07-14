5cf316a7aeb2126ffc64a62bf16ee31d5728f130
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-14 08:18:24 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point to putchar alphapet
+ *main - Entry point
  *
- * Retrun: Always 0 (Success)
+ *Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int chaar;
