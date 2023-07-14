f81df0cff9ef1a74e1a21e3fe5d647607240ddc6
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-14 07:29:04 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success);
+ */
+
+int main(void)
+{
+	int chaar;
+
+	for (chaar = 122; chaar > 96; chaar--)
+		putchar(chaar);
+	putchar('\n');
+return (0);
+}
