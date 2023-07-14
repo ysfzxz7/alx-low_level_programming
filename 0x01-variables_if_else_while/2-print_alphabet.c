5b70424934a4a0d608e891f0fe990986a3d5c075
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-14 06:26:39 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+return (0);
+}
