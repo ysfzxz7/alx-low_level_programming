0ea737aa9ca335771b601e794acbc3b341c582fb
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-14 09:06:55 -0700

    task3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/***
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l,r;
+
+	for (l = 48; l < 58; l++)
+	{
+		for (r = l+1;r < 58; r++)
+		{
+		if (r==l)
+			continue;
+		}
+		putchar(l);
+		putchar(r);
+		if (l == 56 && r == 57)
+	       		continue;	
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+return (0);
+}
