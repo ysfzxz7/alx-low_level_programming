32ce33e64140275b8b52ad95af425f1c118d85e8
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 15:14:45 -0700

    Task101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void){
+	int i,sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}	
+	printf("%d\n",sum);
+	return (0);
+}
