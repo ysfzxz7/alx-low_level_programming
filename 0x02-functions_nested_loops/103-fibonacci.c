c34ec1f9478eb59aaa196dfb4bf167187dee59b4
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 16:05:43 -0700

    Task104

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+int main(void)
+{
+	unsigned long int i, j, sum, subsum, ptr, max;
+	max = 4000000;
+	subsum = 0;
+	i = 0;
+	j = 1;
+	for (ptr = 0; ptr < 50; ptr++)
+	{
+		sum = j + i;
+		i = j;
+		j = sum;
+		if (sum % 2 == 0 && sum < max)
+		{
+			subsum = sum + subsum;
+		}
+	}
+	printf("%lu\n", subsum);
+return(0);
+}
