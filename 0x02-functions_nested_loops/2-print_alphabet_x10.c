1564333948e9f32b32d4ca87a519e08918ce4b2f
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 05:22:37 -0700

    Task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,15 @@
 #include "1-alphabet.c"
 
 /**
- * print_alphabet_x10 - prints alphabet in lower case
+ *print_alphabet_x10 - func that aprint all alphabet in row
+ *
  */
-void print_alphabet_x10(void)
-{
-	int i = 0;
 
-	do {
-		print_alphabet();
-		i++;
-	} while (i < 10);
-}
+	void print_alphabet_x10(void){
+		int i = 0;
+
+		do {
+			print_alphabet();
+			i++;
+		} while (i<10);
+	}
