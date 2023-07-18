8dc5746a4d3656b576d440815225cc8b463db87e
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 07:31:39 -0700

    Task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_abs - thid func return the abs value of an integer
+ *
+ *@n: var needs to be cheked
+ *
+ *Return: n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (n *= -1);
+	}
+	return (n);
+}
