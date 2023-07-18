6d7d03eea8681b66ea64ee4f6f21561d3d14580b
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 11:17:31 -0700

    Task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *add - main func that adds tow intger
+ *
+ *
+ *@a: are first needs to be added
+ *
+ *@b: the second integer
+ *
+ *Return: the sum of the two var
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
