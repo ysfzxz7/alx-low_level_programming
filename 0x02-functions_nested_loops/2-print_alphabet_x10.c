ac106b9e95d9eb6940cb338520543307f6ef5583
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 04:35:02 -0700

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print the alph char *10.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 97, j <= 122, j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,7 @@
 void _putchar(char l);
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
 #endif
 
