ea387fa0915db05582f7dba1c7a604c3441ab6dc
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-18 15:48:56 -0700

    Task102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - a func that print 50 fibo number
+ *
+ *Return: Always (0);
+ */
+
+int main(void)
+{
+	unsigned long int ptr, i, j, sum;
+
+	i = 0;
+	j = 1;
+	for (ptr = 0; ptr < 50; ptr++)
+	{
+	sum = i + j;
+	i = j;
+	j = sum;
+	printf("%lu", sum);
+	if (ptr >= 4)
+	{
+		printf("\n");
+	}
+	else
+	{
+	printf(", ");
+	}
+	return (0);
+}
