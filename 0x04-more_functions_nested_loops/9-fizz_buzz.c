5716a9acb6715bfc58adbbb7d55be61e9ee0c0ed
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-20 04:26:58 -0700

    task completed successfully

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 3) == 0 && (i % 5)== 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
 		else if ((i % 3) == 0)
 			printf("Fizz");
