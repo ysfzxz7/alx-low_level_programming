a70bbda2fc33d11b39fedb0e51d8e88344f3db2b
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-24 07:21:59 -0700

    delete some files

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
  *_strlen - is a func count the number of element in an string
  *
- *
+ *@s: is a string pointer
  */
 
 int _strlen(char *s)
 {
-	int i = 0;
-
-	while(s+i != NULL)
-	{
-	i++;
-	}
-	return(i);
+	return(strlen(s));
 }
