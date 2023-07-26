b49de67321cb13418bd6616a0dede94c4877948b
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-26 11:39:07 -0700

    task 102

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,12 @@
 #include "main.h"
 
 /**
+ *_strncpy - is a func cpy a number of elem
  *
- *
- *
- *
- *
+ *@dest: the dest
+ *@src: the source
+ *@n: the number of elem needs to be copied
+ *Return: the pointer to the dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
