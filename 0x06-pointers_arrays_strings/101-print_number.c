b9de39e2303aa687d040e81277968ef8424bd488
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-26 10:06:24 -0700

    task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_number - a function that print a number
+ *@n: the number need to be checked
+ *
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+	
+	if (n < 0)
+	{
+	
+		num = -n;
+		_putchar('-');
+	}
+	else
+		num = n;
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) +48);
+}
