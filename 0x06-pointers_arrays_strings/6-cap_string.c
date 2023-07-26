fdad5b282c0dfd4c479974f96ed650f2966a89c0
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-26 12:02:48 -0700

    task 6x

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,61 @@
-#include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
- *cap_string - captilize the word
- *@s: is the the string
- *
- *Return the string
+ * is_lower - first func that check if a char is upper
+ * @letter: letter needs to be checked
+ * Return: 1 is lowercase otherwise 0
+ */
+
+int is_lower(int letter)
+{
+	if (letter >= 97 && letter <= 122)
+		return (1);
+	return (0);
+}
+
+/**
+ * is_special - this func check if this char is spesial or not
+ * @letter: character
+ * Return: 1 if special character, 0 ohterwise
+ */
+
+int is_special(char l)
+{
+	if (l == ',' || l == ';'
+		|| l == '.' || l == '!'
+		|| l == '?' || l == '\"'
+		|| l  == '(' || l == ')'
+		|| l == '{' || l == '}'
+		|| l == ' ' || l == '\t' || l == '\n'
+	   )
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes the words
+ * @s: string included
+ * Return: pointer of string 
  */
 
 char *cap_string(char *s)
-{	
-	int counter = 0;	
-	while (s[counter] != '\n')
+{
+	char *a = s, current;
+	int i, len = strlen(s);
+
+	for (i = 0; i < len; i++)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-		{
-			s[0] = s[0] - 32;
-		}
-		if (s[counter] == ' ' || s[counter] == '\t' ||
-		s[counter] == '\n' || s[counter] == ',' || s[counter] == ';' ||
-		s[counter] == '.' || s[counter] == '!' || s[counter] == '?' ||
-		s[counter] == '"' || s[counter] == '(' || s[counter] == ')' ||
-		s[counter] == '{' || s[counter] == '}')
-		{
-			if (s[counter + 1] >= 97 && s[counter + 1] <= 122)
-			{
-				s[counter + 1] = s[counter + 1] - 32 ;
-			}
-		}
-		counter++;
+		current = a[i];
+
+		if (i > 0 && is_lower(current) && is_special(a[i - 1]))
+			a[i] -= 32;
+		else if (i == 0 && is_lower(current))
+			a[i] -= 32;
 	}
+
 	return (s);
 }
