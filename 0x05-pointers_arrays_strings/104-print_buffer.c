f13345ddf00768220c24df28fa9f623e3dcaa01d
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-27 08:18:46 -0700

    task 104

---
+++ 0x05-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_buffer - this func is used to print a given string 
+ * @b: pointer to the string
+ * @size: size of the string
+ */
+
+void print_buffer(char *b, int size)
+{
+	int o = 0, j, i;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i += 2)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if ((i + 1) < j)
+				printf("%02x ",
+					*(b + o + i + 1));
+			else
+				printf("   ");
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (isprint(c))
+				printf("%c", c);
+			else
+				printf(".");
+		}
+		printf("\n");
+		o += 10;
+	}
+}
