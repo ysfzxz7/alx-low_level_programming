66519b9a55d39679856941d0679df4a7ad370c51
ysfzxz7 <bbbmaroc83@gmail.com>
2023-07-31 06:44:08 -0700

    task1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@ int _putchar(char c);
 /*============= _memset============*/
 char *_memset(char *s, char b, unsigned int n);
 
-/*=====*/
+/*====_memcpy=======*/
+char *_memcpy(char *dest, char *src, unsigned int n);
 
+/*======*/
 #endif
