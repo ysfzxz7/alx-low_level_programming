43653f0370dbce75680d56ee55702e1be396ea69
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-02 02:17:39 -0700

    task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings using recursion
+ * @s1: first str
+ * @s2: second str
+ * Return: 1 if the same otherwise 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	//check if s2 reaching the end
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+
+	if (*s1 == *s2)
+		return (
+			*s2 != '\0' &&
+			wildcmp(s1 + 1, s2 + 1)
+		);
+
+	if (*s2 == '*')
+		return (
+			wildcmp(s1, s2 + 1) ||
+			(*s1 != '\0' && wildcmp(s1 + 1, s2))
+		);
+
+	return (0);
+}
