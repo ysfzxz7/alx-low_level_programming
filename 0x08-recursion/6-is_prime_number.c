381d5f1c47e5a21e3fe7fbcd319e8f226db3c246
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-02 02:31:09 -0700

    task 5

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* is_prime_number - check if a number is prime or not
+*@n: number needs to check
+*
+*Return: true if number if prime
+*/
+int is_prime_number(int n)
+{
+	int i = n / 2;
+
+	if (n <= 1)
+		return (0);
+	return (is_prime_nbr(n, i));
+}
+
+/**
+ * is_prime_nbr - func used to find the prim number
+ * @n: first nbr
+ * @g: what is the nbr
+ * Return: 1 if number is prime otherwide 0
+ */
+
+int is_prime_nbr(int x, int y)
+{
+	if (x % y == 0 && y != 1)
+		return (0);
+	else if (y == 1)
+		return (1);
+	return (prime(x, y - 1));
+}
