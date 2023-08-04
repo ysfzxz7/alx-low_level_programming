8371a245289a8a3d6594e483ca115196159d41ba
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-04 05:07:26 -0700

    task2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ *main - the entry point
+ *@argc: command counter
+ *@argv: array of command
+ *Return: 1 if error otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{	printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1] * atoi(argv[2]);
+	return (0);
+}
