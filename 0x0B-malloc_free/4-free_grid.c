f58d6d9335b5b29538e1092c31555b9cff82e662
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-07 06:10:53 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees a grid ptrs.
+ * @grid: double  ptr array.
+ * @height: height of the arraay
+ * 
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
