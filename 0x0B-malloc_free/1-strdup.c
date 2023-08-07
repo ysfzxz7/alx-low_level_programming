49f5eed5eecf1be11dfc2295834ce16ae1c85af7
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-07 05:57:53 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,27 +3,30 @@
 #include <string.h>
 
 /**
- * _strdup - a func that dublicat a str
+ * _strdup - a func that dub a  string
  * @str: the given string
- * Return: the pointer returned
+ * Return: the pointer to the new string
  */
 
 char *_strdup(char *str)
 {
-	int i, len = strlen(str);
 	char *ptr;
+	int len, i = 0;
 
-	i = 0;
 	if (str == NULL)
 		return (NULL);
-	ptr = malloc(sizeof(char) * len + 1);
+
+	len = strlen(str);
+	ptr = malloc(len * sizeof(char) + 1);
 	if (ptr == NULL)
 		return (NULL);
+
 	while (i < len)
 	{
 		ptr[i] = str[i];
 		i++;
 	}
 	ptr[i] = '\0';
+
 	return (ptr);
 }
