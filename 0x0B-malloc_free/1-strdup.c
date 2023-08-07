2317005402ea0a71475ac35c82f200562d1edbfc
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-07 05:50:43 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - a func that dublicat a str
+ * @str: the given string
+ * Return: the pointer returned
+ */
+
+char *_strdup(char *str)
+{
+	int i, len = strlen(str);
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(char) * len + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
