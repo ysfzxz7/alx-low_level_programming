a6ed0703a62f6c30460addbb54004e8ebc95577a
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-14 07:38:45 -0700

    init and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - is is a struct to creat a dog
+ *@name: the name of the dog's
+ *@age: the age of the dog
+ * @owner: the name of the owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
