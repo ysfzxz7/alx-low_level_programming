a614e34b5fd8ce1971c82959a66c485f10389df5
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-14 06:58:48 -0700

    init and task 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,8 @@
+#ifndef SUM_H
+#define SUM_H
+
 #ifndef SUM
-#define SUM(x,y) ((x) + (y))
-#endif
+#define SUM(x, y) ((x) + (y))
+#endif /* this for the SUM macro*/
+
+#endif /*this for the guard of the Macro*/
