39e6617fb0e067b6d4843b6e37946efb7b8e38d9
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-16 04:57:38 -0700

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ *array_iterator - a func that iterate over a array of func
+ *@array: the array of argument
+ *@size: the size of the array
+ *@action: is a pointer to the func to use
+ *
+ *Return: nothing , just print some data
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,21 @@
+#include <stddef.h>
+
 #ifndef FUNC_PTR
 #define FUNC_PTR
+/**
+ *in task 0 -> the print_name function used
+ *to call two fucntions depending on the user
+ *needs, the first one print the name as it is
+ *the seconde uppercase it
+ */
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+
+/**
+ *this is the prototype of a function that exx=cute the
+ *a given function iterating on a on a array of argumnt
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif
