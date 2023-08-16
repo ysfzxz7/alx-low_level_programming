0dd168ee70e23b576a8f57b6ae684bbf400eaf49
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-16 10:43:07 -0700

    the last task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - the Entry  point
+ *@argc: argument counter
+ *@argv: the aray of variables
+ *
+ *Return: allways 0;
+ */
+
+int main(int argc, char **argv)
+{
+	int byte, i;
+	int (*address)(int, char **) = main;
+	unsigned char operator_code;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	byte = atoi(argv[1]);
+
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < byte - 1; i++)
+	{
+	operator_code = *(unsigned char *)address;
+	printf("%.2x", operator_code);
+	if (i == byte - 1)
+	{
+		continue;
+		printf(" ");
+		address++;
+	}
+	printf("\n");
+	return (0);
+
+	}
+}
