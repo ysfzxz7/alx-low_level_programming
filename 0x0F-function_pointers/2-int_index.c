52d5790bbe2dfd5db47ac3fc1f08fc896ee480c1
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-16 05:40:03 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,10 +18,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (!cmp(array[i]))
+		if (!cmp(array[i]) && (i + 1) == size)
+		{
 			return (-1);
+		}
 		else if (cmp(array[i]))
 			return (i);
 	}
+
 	return (i);
 }
