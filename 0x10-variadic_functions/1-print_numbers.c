772b1bc16b865dd700efe1e664721cd5ad5a1fe8
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-17 06:22:54 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list  args;
 
-	if (separator == NULL)
-		return;
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
