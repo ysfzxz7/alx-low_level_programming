a460fc4aa59337e85a4627fcd4e637ec583855d1
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-17 08:45:20 -0700

    last 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,11 @@
-#ifndef VAR_H
-#define VAR_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
 #include <stdio.h>
 
-typedef struct pr{
-char *sym;
+typedef struct printer{
+char *symbol;
 void (*print)(va_list arg);
 } printer_t;
 
@@ -12,5 +13,10 @@ void (*print)(va_list arg);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 void print_all(const char * const format, ...);
+
 #endif
