bbc95621ee70e8ff5105d0a5bd460e8e306b4380
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-28 11:10:09 -0700

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *curr = head;
 
 	if (!head)
-		return (NULL);
+		return (0);
 
 	while (curr && i++ < index)
 		curr = curr->next;
