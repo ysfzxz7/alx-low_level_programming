fbfce9a9420be8d576df70e8e31fb38b471717ba
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-28 10:25:14 -0700

    first

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *free_listint - a func that frees all the linked list
+ *@head: the head of the linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (!head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+
+}
