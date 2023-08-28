63e310419968603ecbd13b0f516d610ed9f43433
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-28 11:20:10 -0700

    task7

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *sum_listint - a func that return the sum of a linked list
+ *@head: the head of the linked list
+ *Return: the geld collected
+ *
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int geld = 0;
+	listint_t curr = head;
+
+	while(curr)
+	{
+		geld += curr->n;
+		curr = curr->next;
+	}
+	return (geld);
+}
