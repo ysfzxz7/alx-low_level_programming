be45afd1a8750fbe22d2b214c5928ca46197c8da
ysfzxz7 <bbbmaroc83@gmail.com>
2023-08-29 02:59:49 -0700

    added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (ptr && i++ < idx)
 		ptr = ptr->next;
 	if (!ptr)
-		return (NULL);
+	{
+		(*head)->next = new;
+		new->next = NULL;
+	}
 	tmp = ptr->next;
 	ptr->next = new;
 	new->next = tmp;
