1cac07797400bb095a5afbd68a1ab10e423539e8
ysfzxz7 <bbbmaroc83@gmail.com>
2023-09-04 08:22:49 -0700

    first shoot

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filed = open(filename, O_CREAT | O_RDWR | O_TRUNC, 06000);
+	filed = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (filed == -1)
 		return (-1);
 	if (text_content)
