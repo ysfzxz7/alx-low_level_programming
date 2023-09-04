a903edd1e1ae39b6a58f974860c61c5092022430
ysfzxz7 <bbbmaroc83@gmail.com>
2023-09-04 08:30:25 -0700

    first shoot

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - apped some text to an existing file
+ *@filenalme: the file name
+ *@text_content: the text wants to be added
+ *Retrun: if success 1 otherwise -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int filed, lenght = 0;
+
+	if (!filename)
+		return (-1);
+	filed = open(filename, O_WRONLY | O_APPEND);
+	if (text_content)
+		for (lenght = 0; text_content[lenght]; )
+			lenght++;
+	if (write(filed, text_content, lenght) == -1)
+		return (-1);
+	close(filed);
+	return (1);
+}
