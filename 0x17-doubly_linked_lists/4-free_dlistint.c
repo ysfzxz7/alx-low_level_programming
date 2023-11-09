e9c24c89d0490c9eb3de693df55bcf04c1de8b0d
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-09 06:49:59 -0800

    push

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *free_dlistint - a func that frees the linked list
+ *@head: the head ptr of the linked list
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr = head;
+	dlistint_t *next;
+
+	if (!head)
+		return;
+	while(curr)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+	}
+}
