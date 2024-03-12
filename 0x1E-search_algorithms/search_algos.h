2548b8867e0883c054507d4591a1bd04f16c35a3
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-12 09:33:51 -0700

    this a fiuel

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,14 +1,9 @@
-#ifndef _SEARCH_ALGOS_H_
-#define _SEARCH_ALGOS_H_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-/*================= Header files ===================*/
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-
-/*================= Structures ===================*/
 
 /**
  * struct listint_s - singly linked list
@@ -18,12 +13,13 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+		int n;
+		size_t index;
+		struct listint_s *next;
 } listint_t;
 
 /**
@@ -34,18 +30,15 @@ typedef struct listint_s
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure with an express lane
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
-
-
-/*================= Prototypes ===================*/
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -53,16 +46,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-
 listint_t *jump_list(listint_t *list, size_t size, int value);
-void print_list(const listint_t *list);
-void free_list(listint_t *list);
-listint_t *create_list(int *array, size_t size);
-
 skiplist_t *linear_skip(skiplist_t *list, int value);
-skiplist_t *create_skiplist(int *array, size_t size);
-void free_skiplist(skiplist_t *list);
-void print_skiplist(const skiplist_t *list);
 
+#endif /* SEARCH_ALGOS_H */
 
-#endif /* _SEARCH_ALGOS_H_ */
